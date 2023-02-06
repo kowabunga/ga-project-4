5618bbf9953d44d72d96616fab6fb3a1b5b394a6
Anthony Siletti <asiletti@icloud.com>
2023-02-05 21:14:41 -0500

    quick homepage update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,16 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section>
+      <div className='container-fluid'>
+        <h1 className='display-5 fw-bold'>Welcome to Master Cook</h1>
+        <p className='lead'>
+          Master Cook enables you to share your favorite recipes and tips.
+          Create your favorite recipes and let the world feast.{' '}
+        </p>
+        <p className='lead'>
+          Don't forget to share your favorite kitchen tips and tricks too!
+        </p>
+      </div>
+    </section>
+  );
 }
